0f911c96847b08380598e9e4219a77df649f7ff7
Jorge Luis Freitas Costa <202119060212@lab02-l>
2023-05-22 14:05:54 -0400

    feat: add function implemented

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
+from add import add
+
 a = float(input("Insira um número: "))
 b = float(input("Insira outro número: "))
+
 print("Opa, vc digitou um {} e um {}".format(a, b))
+print("E acontece que a soma deles é {}".format(add(a, b)))
