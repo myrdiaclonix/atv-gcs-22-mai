9b612f33ebefd389b1212b6aa1ffa080af47f9f3
Jorge Luis Freitas Costa <202119060212@lab02-l>
2023-05-22 14:35:28 -0400

    feat: user check working in default

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
-a = int(input("Insira um número: "))
-print("Opa, vc digitou um {}".format(a))
+from test import test
+
+def main():
+    if test():
+        a = int(input("\nInsira um número: "))
+        print("Opa, vc digitou um {}".format(a))
+    else:
+        print("Vc n passou no teste! ROBÔ!!!")
+
+if __name__ == "__main__":
+    main()
